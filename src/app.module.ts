ebac6b2ea110243352182e6ea22dcf45c3da1080
hobiJeong <jjb26433@naver.com>
2023-08-23 17:56:32 +0900

    feat(#1): S3 연동폴더 images 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeORMconfig } from './config/typeorm.config';
 import { Test1Controller } from './test1/test1.controller';
 import { Test1Service } from './test1/test1.service';
 import { Test1Module } from './test1/test1.module';
+import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeORMconfig), Test1Module],
+  imports: [TypeOrmModule.forRoot(TypeORMconfig), Test1Module, ImagesModule],
   controllers: [Test1Controller],
   providers: [Test1Service],
 })
