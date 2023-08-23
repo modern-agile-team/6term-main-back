9232b3bd6782e9c3219b6438ae2362db9eb105aa
hobiJeong <jjb26433@naver.com>
2023-08-23 18:00:55 +0900

    feat(#1): 모듈,컨트롤러,서비스 import

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { TypeORMconfig } from './config/typeorm.config';
 import { Test1Controller } from './test1/test1.controller';
 import { Test1Service } from './test1/test1.service';
 import { Test1Module } from './test1/test1.module';
+import { ImagesController } from './images/images.controller';
+import { ImagesService } from './images/images.service';
 import { ImagesModule } from './images/images.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(TypeORMconfig), Test1Module, ImagesModule],
-  controllers: [Test1Controller],
-  providers: [Test1Service],
+  controllers: [Test1Controller, ImagesController], 
+  providers: [Test1Service, ImagesService], 
 })
 export class AppModule {}
