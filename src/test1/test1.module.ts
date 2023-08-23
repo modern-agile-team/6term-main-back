9782d7943bb536a9bd039d3428c0b672ac275a1c
2swo <swlee456@naver.com>
2023-08-23 17:50:09 +0900

    refector(#1) : develop 환경변수 설정 초기

--- src/test1/test1.module.ts
+++ src/test1/test1.module.ts
@@ -3,7 +3,7 @@ import { Test1Controller } from './test1.controller';
 import { Test1Service } from './test1.service';
 
 @Module({
-  // clntroller와 service가 모듈에 정의되어야 컨트롤러,서비스 안에서 이용가능합니다.
+  // controller와 service가 모듈에 정의되어야 컨트롤러,서비스 안에서 이용가능합니다.
   controllers: [Test1Controller],
   providers: [Test1Service],
 })
