702503c9b2474071a51a58e37e00335622d564c3
hobiJeong <jjb26433@naver.com>
2023-08-26 15:35:52 +0900

    feat(#1): 다중 업로드 API uploads/multi로 수정

--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -12,14 +12,14 @@ import { UploadsService } from './uploads.service';
 export class UploadsController {
   constructor(private uploadService: UploadsService) {}
 
-  // @Post()
-  // @UseInterceptors(FileInterceptor('file'))
-  // async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<string> {
-  //   // 반환 형식을 { url: string } 에서 string 로 변경
-  //   return this.uploadService.uploadFile(file); // uploadService.uploadFile의 반환 값을 그대로 반환
-  // }
   @Post()
-  @UseInterceptors(FilesInterceptor('files', 2)) 
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<string> {
+    // 반환 형식을 { url: string } 에서 string 로 변경
+    return this.uploadService.uploadFile(file); // uploadService.uploadFile의 반환 값을 그대로 반환
+  }
+  @Post('/multi')
+  @UseInterceptors(FilesInterceptor('files', 3)) 
   async uploadFiles(@UploadedFiles() files: Express.Multer.File[]): Promise<string[]> {
     try {
       return await this.uploadService.uploadFiles(files);
