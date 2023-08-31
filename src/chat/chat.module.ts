a6cf8784c399aaffe4dd035aa3b7ef8bcdb3d8a3
hobiJeong <jjb26433@naver.com>
2023-08-31 18:04:25 +0900

    feat(#1): module schema 추가

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ChatRoomSchema } from './schemas/chat-room.schemas';
+import { ChatRoom, ChatRoomSchema } from './schemas/chat-room.schemas';
+import { Chat, ChatSchema } from './schemas/chat.schemas';
+import { ChatImage, ChatImageSchema } from './schemas/chat-image.schemas';
+import { ChatNotification, ChatNotificationSchema } from './schemas/chat-notifiation.schemas';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name:'ChatRoom', schema: ChatRoomSchema}])],
+  imports: [MongooseModule.forFeature([
+    { name: ChatRoom.name , schema: ChatRoomSchema },
+    { name: Chat.name , schema: ChatSchema },
+    { name: ChatImage.name, schema: ChatImageSchema }, 
+    { name: ChatNotification.name, schema: ChatNotificationSchema },
+  ])],
   controllers: [ChatController],
   providers: [ChatService]
 })
