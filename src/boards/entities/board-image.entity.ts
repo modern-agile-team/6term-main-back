6264cfc3f3892f57167c988881e67c82f305bf5c
hobiJeong <jjb26433@naver.com>
2023-09-05 11:52:18 +0900

    feat(#1): board-image.entity add

---
+++ src/boards/entities/board-image.entity.ts
@@ -0,0 +1,16 @@
+import { CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Board } from "./board.entity";
+
+@Entity({ name: 'board_image' })
+
+export class BoardImage {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @ManyToOne(() => Board)
+    @JoinColumn({ name: 'board_id '})
+    boardId: Board;
+
+    @CreateDateColumn({ name: 'create_at' })
+    createAt: Date;
+}
