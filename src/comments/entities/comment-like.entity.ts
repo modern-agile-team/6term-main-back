6c8b5dfe2bad3f74b213d1a398a435a4747da569
hobiJeong <jjb26433@naver.com>
2023-09-05 12:18:00 +0900

    feat(#1): comment-like.entity add

---
+++ src/comments/entities/comment-like.entity.ts
@@ -0,0 +1,24 @@
+import { User } from "src/users/entities/user.entity";
+import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { Comment } from "./comment.entity";
+import { Recomment } from "./recomment.entity";
+
+@Entity({ name: 'comment_like' })
+
+export class CommentLike {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'user_id' })
+    userId: User;
+
+    @ManyToOne(() => Comment)
+    @JoinColumn({ name: 'board_id' })
+    boardId: Comment;
+
+    @ManyToOne(() => Recomment)
+    @JoinColumn({ name: 'recomment_id' })
+    recommentId: Recomment;
+
+}   
