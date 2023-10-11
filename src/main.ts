bd72782ae4289b9717b5c57ceb1be497fab031d8
hobiJeong <jjb26433@naver.com>
2023-10-11 14:21:35 +0900

    refactor(#3): 서비스, 레포지터리 분리 및 에러핸들링, User데코레이터 적용

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger';
-import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const logger = new Logger();
+  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   setupSwagger(app);
   app.useLogger(logger);
