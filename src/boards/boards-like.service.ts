e0964b0b267abbd3730bc9f99ab7f746e6628939
hobiJeong <jjb26433@naver.com>
2023-10-12 17:38:00 +0900

    feat(#27): 게시글 좋아요 개수 카운팅 구현

--- src/boards/boards-like.service.ts
+++ src/boards/boards-like.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { EntityManager } from 'typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { EntityManager, Equal } from 'typeorm';
 import { BoardLike } from './entities/board-like.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Board } from './entities/board.entity';
@@ -13,9 +13,17 @@ export class BoardsLikeService {
       where: { id: boardId },
     });
 
+    if (!board) {
+      throw new NotFoundException('해당 게시글이 없습니다.');
+    }
+
     const user = await this.entityManager.findOne(User, {
       where: { id: userId },
     });
+
+    if (!user) {
+      throw new NotFoundException('해당 유저가 없습니다');
+    }
 
     const boardLike = new BoardLike();
     boardLike.boardId = board;
@@ -28,9 +36,18 @@ export class BoardsLikeService {
     const board = await this.entityManager.findOne(Board, {
       where: { id: boardId },
     });
+
+    if (!board) {
+      throw new NotFoundException('해당 게시글이 없습니다.');
+    }
+
     const boardLike = await this.entityManager.find(BoardLike, {
-      where: { boardId: board },
+      where: { boardId: Equal(boardId) },
     });
+
+    console.log(boardLike);
+    console.log(boardLike.length);
+
     return boardLike.length;
   }
 }
