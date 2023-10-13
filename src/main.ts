a5fa1684d34546f2e6e6090d7abd86d37bae9a77
hobiJeong <jjb26433@naver.com>
2023-10-13 10:33:47 +0900

    feat(#34): HttpExceptionFilter 적용

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './config/swagger';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { HttpExceptionFilter } from './http-Exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const logger = new Logger();
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   setupSwagger(app);
   app.useLogger(logger);
