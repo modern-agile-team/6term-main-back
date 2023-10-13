6dc30e4fe91157ca7e62e223f6237303242a09e9
NicoDora <pjh76574734@gmail.com>
2023-10-13 14:51:27 +0900

    refactor(#10): 유저 이미지 명세서 작성

--- src/users/controllers/user-image.controller.ts
+++ src/users/controllers/user-image.controller.ts
@@ -2,7 +2,7 @@ import { TokenService } from './../../auth/services/token.service';
 import { Controller, Headers, Patch, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UserImageService } from '../services/user-image.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('user/image')
 @ApiTags('user API')
@@ -13,6 +13,9 @@ export class UserImageController {
     ) {}
 
   @ApiOperation({ summary: '유저 이미지 업로드 API', description: '유저 이미지 업로드 API' })
+  @ApiResponse({ status: 200, description: '성공적으로 이미지를 업로드한 경우', content: { JSON: { example: { message: '이미지 업로드에 성공했습니다.' } } } })
+  @ApiResponse({ status: 500, description: '이미지 업로드 및 처리 중 오류가 발생한 경우', content: { JSON: { example: { statusCode: 500, message: '이미지 업로드 및 처리 중 오류가 발생했습니다.' } } } })
+  @ApiResponse({ status: 403, description: '만료된 액세스 토큰인 경우', content: { JSON: { example: { statusCode: 403, message: '만료된 토큰입니다.' } } } })
   @Patch()
   @UseInterceptors(FileInterceptor('file'))
   async uploadImage(@Headers('access_token') accessToken: string, @UploadedFile() file: Express.Multer.File) {
