07b197021880f30d0c968c6ce2a4f402cbd94412
hobiJeong <jjb26433@naver.com>
2023-10-16 17:34:38 +0900

    feat(#33): swagger추가 및 ValidationPipe 적용

--- src/boards/controllers/boards-like.controller.ts
+++ src/boards/controllers/boards-like.controller.ts
@@ -5,15 +5,21 @@ import {
   Param,
   ParseIntPipe,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { BoardsLikeService } from '../services/boards-like.service';
 import { Users } from 'src/common/decorators/user.decorator';
 import { User } from 'src/users/entities/user.entity';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('BOARDS-LIKE')
+@UsePipes(ValidationPipe)
 @Controller('boards')
 export class BoardsLikeController {
   constructor(private boardsLikeService: BoardsLikeService) {}
 
+  @ApiOperation({ summary: '좋아요 생성' })
   @Post('like/:boardId')
   async addBoardLike(
     @Users() user: User,
@@ -22,11 +28,13 @@ export class BoardsLikeController {
     return await this.boardsLikeService.addBoardLike(boardId, user.id);
   }
 
+  @ApiOperation({ summary: '해당 게시글 좋아요 개수 조회' })
   @Get('like/:boardId')
   async getBoardLike(@Param('boardId', ParseIntPipe) boardId: number) {
     return await this.boardsLikeService.getBoardLike(boardId);
   }
 
+  @ApiOperation({ summary: '해당 게시글 좋아요 삭제' })
   @Delete('like/:boardId')
   async deleteBoardLike(
     @Users() user: User,
