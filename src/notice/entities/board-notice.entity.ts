739779b51a0f4b5c0bcf625a93f00fdb795bdea4
hobiJeong <jjb26433@naver.com>
2023-10-19 21:17:12 +0900

    feat(#39): board-notice entity 수정

--- src/notice/entities/board-notice.entity.ts
+++ src/notice/entities/board-notice.entity.ts
@@ -1,4 +1,5 @@
 import { Board } from 'src/boards/entities/board.entity';
+import { User } from 'src/users/entities/user.entity';
 import {
   CreateDateColumn,
   Entity,
@@ -15,6 +16,14 @@ export class BoardNotification {
   @ManyToOne(() => Board)
   @JoinColumn({ name: 'board_id' })
   boardId: Board;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'sender_id' })
+  senderId: User;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'receiver_id' })
+  receiverId: User;
 
   @CreateDateColumn({ name: 'create_at' })
   createAt: Date;
