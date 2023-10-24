a20661c3815833ad1379f5575157e702a5a8ebab
NicoDora <pjh76574734@gmail.com>
2023-10-24 18:16:00 +0900

    feat(#11): 유저 이름, 이미지 제공 API 명세서 추가

--- src/users/controllers/user.controller.ts
+++ src/users/controllers/user.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { UserService } from "../services/user.service";
+import { ApiGetUserInfo } from "../swagger-decorators/get-user-info.decorator";
+import { ApiTags } from "@nestjs/swagger";
 
 @Controller('user')
+@ApiTags('user API')
 export class UserController {
   constructor(
     private readonly userService: UserService,
   ) {}
 
+  @ApiGetUserInfo()
   @Get('info/:user_id')
   async getUserInfo(@Param('user_id') userId: number) {
     return this.userService.getUserInfo(userId);
